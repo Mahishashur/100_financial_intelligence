ad33a4db5ace063cd454340bbaf6d24ba3d22c2c
Mahishashur <yashimanshusingh@gmail.com>
2026-06-29 21:25:52 +0530

    Sprint2

--- src/analytics/ratios.py
+++ src/analytics/ratios.py
@@ -4,7 +4,6 @@ def calculate_npm(sales, net_profit):
 
     if sales <= 0:
         return None
-
     return round((net_profit / sales) * 100,2)
 
 # Operating_profit_margin
@@ -13,7 +12,6 @@ def calculate_opm(sales, operating_profit):
 
     if sales <= 0:
         return None
-
     return round((operating_profit / sales) * 100,2)
 
 # Return_on_equity
@@ -21,10 +19,8 @@ def calculate_opm(sales, operating_profit):
 def calculate_roe(net_profit,equity_capital,reserves):
 
     total_equity = equity_capital + reserves
-
     if total_equity <= 0:
         return None
-
     return round((net_profit / total_equity) * 100,2)
 
 # Return_on_capital_employed
@@ -37,12 +33,9 @@ def calculate_roce(
 ):
 
     capital = (equity_capital + reserves + borrowings)
-
     if capital <= 0:
         return None
-
     return round((operating_profit / capital) * 100,2 )
-
 
 # Return_on_assests
 
@@ -50,5 +43,4 @@ def calculate_roa(net_profit,total_assets):
 
     if total_assets <= 0:
         return None
-
     return round((net_profit / total_assets) * 100,2)
