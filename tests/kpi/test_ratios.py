44f49be177999dc81a68c53d623bde18e446935b
Mahishashur <yashimanshusingh@gmail.com>
2026-06-29 20:38:01 +0530

    Sprint_2

--- tests/kpi/test_ratios.py
+++ tests/kpi/test_ratios.py
@@ -9,45 +9,27 @@ from src.analytics.ratios import (
 
 def test_npm():
 
-    assert calculate_npm(
-        1000,
-        100
-    ) == 10
+    assert calculate_npm(1000,100) == 10
 
 
 def test_opm():
 
-    assert calculate_opm(
-        1000,
-        200
-    ) == 20
+    assert calculate_opm(1000,200) == 20
 
 
 def test_roe():
 
-    assert calculate_roe(
-        100,
-        200,
-        300
-    ) == 20
+    assert calculate_roe(100,200,300) == 20
 
 
 def test_roce():
 
-    assert calculate_roce(
-        250,
-        100,
-        400,
-        500
-    ) == 25
+    assert calculate_roce(250,100,400,500) == 25
 
 
 def test_roa():
 
-    assert calculate_roa(
-        120,
-        1000
-    ) == 12
+    assert calculate_roa(120,1000) == 12
     
     
 import pytest
@@ -60,10 +42,7 @@ from src.analytics.ratios import (
     calculate_roa
 )
 
-
-# ----------------------------
 # NPM
-# ----------------------------
 
 def test_npm():
     assert calculate_npm(1000, 100) == 10
@@ -72,10 +51,7 @@ def test_npm():
 def test_npm_zero_sales():
     assert calculate_npm(0, 100) is None
 
-
-# ----------------------------
 # OPM
-# ----------------------------
 
 def test_opm():
     assert calculate_opm(1000, 200) == 20
@@ -84,10 +60,7 @@ def test_opm():
 def test_opm_zero_sales():
     assert calculate_opm(0, 200) is None
 
-
-# ----------------------------
 # ROE
-# ----------------------------
 
 def test_roe():
     assert calculate_roe(100, 200, 300) == 20
@@ -96,10 +69,7 @@ def test_roe():
 def test_roe_negative_equity():
     assert calculate_roe(100, -200, 100) is None
 
-
-# ----------------------------
 # ROCE
-# ----------------------------
 
 def test_roce():
     assert calculate_roce(250, 100, 400, 500) == 25
@@ -108,10 +78,7 @@ def test_roce():
 def test_roce_zero_capital():
     assert calculate_roce(250, 0, 0, 0) is None
 
-
-# ----------------------------
 # ROA
-# ----------------------------
 
 def test_roa():
     assert calculate_roa(120, 1000) == 12
